9c2319bc44667dabf96b52075dced53a4b8c027d
billy <billy.Tsag@gmail.com>
2019-05-21 14:15:19 +0300

    Adds Scaffolding

---
+++ ERS-Demo-Project/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ERS_Demo_Project.Startup))]
+namespace ERS_Demo_Project
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
